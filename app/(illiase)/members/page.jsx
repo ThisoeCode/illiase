52abe0f4341db4463e95c09eca2fd01a67f74c7d
Thisoe <thisoecode@gmail.com>
2023-12-20 22:42:09 +0900

    [0.1.7.1] Fetch members' list from Atlas

--- app/(illiase)/members/page.jsx
+++ app/(illiase)/members/page.jsx
@@ -1,25 +1,38 @@
 import Link from "next/link"
 import Image from "next/image"
+import { insu } from "@/app/_serve/insu"
 
 export const metadata = {
   title: 'Membra',
 }
 
-export default async function Illiase({memData}) {
-  return (
-  <section id="mem">
-    <Link href='/bn' style={{background: 'linear-gradient(to right, #75ff7f, #85ffaa)'}}>
-      <Image
-        src="/ava/bn.png"
-        width='144' height='144'
-        alt='BN avatar' />
-      <article>
-        <div>
-          <h1>BN</h1>
-          <h2>#群主</h2>
-        </div>
-      </article>
-    </Link>
-  </section>
-  )
+
+const getMem = async ()=>{
+  const {_,db}=await insu()
+  return db.collection('mem')
+    .find({}, {projection: { u: 1, displayName: 1, title:1, cardColor: 1, _id: 0 }} ).toArray()
+}
+
+export default async function Illiase() {
+  /** @type {Array} */
+  const mems = await getMem()
+  console.log(mems)
+  return (<>
+  {mems.map((data,index)=>{
+    <section id="mem" key={index}>
+      <Link href={'/'+data.u} style={{background: `linear-gradient(to right, ${data.cardColor})`}}>
+        <Image
+          src={"/ava/"+data.u+".png"}
+          width='144' height='144'
+          alt={data.displayName+"'s avatar"} />
+        <article>
+          <div>
+            <h1>{data.displayName}</h1>
+            <h2>#{data.title}</h2>
+          </div>
+        </article>
+      </Link>
+    </section>
+  })}
+  </>)
 }
